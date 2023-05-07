bd52279725fd2c4b4dd3f446157c504db73554d7
jt548 <jt548@sussex.ac.uk>
2023-05-07 13:57:08 +0800

    source code

---
+++ assignment/application/index/view/index/index.php
@@ -0,0 +1,104 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>Home</title>
+<link rel="stylesheet" href="__STATIC__/index/css/bootstrap.min.css">
+<link rel="stylesheet" href="__STATIC__/index/css/all.min.css">
+<link rel="stylesheet" href="__STATIC__/index/css/animate.css">
+<link rel="stylesheet" href="__STATIC__/index/css/magnific-popup.css">
+<link rel="stylesheet" href="__STATIC__/index/css/owl.min.css">
+<link rel="stylesheet" href="__STATIC__/index/css/flaticon.css">
+<link rel="stylesheet" href="__STATIC__/index/css/odometer.css">
+<link rel="stylesheet" href="__STATIC__/index/css/main.css">
+</head>
+<body>
+<div class="overlay"></div>
+<a href="#0" class="scrollToTop"><i class="fas fa-angle-up"></i></a>
+<div class="preloader">
+  <div class='loader'></div>
+</div>
+<!-- === Header Section === -->
+<header class="header-section">
+  <div class="container">
+    <div class="header-wrapper">
+      <ul class="menu ml-lg-auto">
+        <li><a href="{:url('/index')}" class="aaaa">Home</a></li>
+        <li><a href="{:url('/about')}">About</a></li>
+        <li><a href="{:url('/originalStatement')}">Original statement</a></li>
+        <li><a href="{:url('/reference')}">reference</a></li>
+        <li><a href="{:url('/cocaCola')}" class="custom-button">X3D Models</a></li>
+      </ul>
+      <div class="header-bar"><span></span><span></span><span></span></div>
+    </div>
+  </div>
+</header>
+<!-- === Header Section === --><!-- === Banner Section === -->
+<section class="banner-section bg_img" data-background="__STATIC__/index/images/banner/banner-bg.jpg">
+  <div class="banner-shape shape-1"></div>
+  <div class="banner-shape shape-2"></div>
+  <div class="banner-shape shape-3"></div>
+  <div class="banner-shape shape-4"></div>
+  <div class="banner-shape shape-5"></div>
+  <div class="banner-shape shape-6"></div>
+  <div class="container" style="margin-top: 140px">
+    <div class="banner-content wow fadeInLeft" data-wow-duration="1s">
+      <h1 class="title"><span class="text-theme d-block">Coca-Cola</span>Family</h1>
+      <a href="{:url('/cocaCola')}" class="custom-button">X3D Models</a></div>
+    <div class="banner-thumb wow fadeInUp" data-wow-delay=".5s" data-wow-duration="1s"><img style="width: 650px;" src="__STATIC__/index/images/banner/banner1.png" alt="banner"></div>
+  </div>
+</section>
+
+
+<section>
+
+    <div class="row" style="margin:0 auto;width: 50%;">
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="__STATIC__/index/ddd.png" alt="..." width="60%" height="30%" style="margin-left: 30px">
+                <div class="caption" style="width: 60%;padding-left: 30px;margin-top: 10px;margin-bottom: 50px">
+                    <p style=""><h5>Fanta</h5>Bright, bubbly, instantly refreshing and great tasting. Fanta is made with 100% natural flavours and is caffeine free. Fanta is available in a variety of real fruit flavours.</p>
+                    <p style="padding-left: 30px"><a href="https://www.coca-cola.co.uk/brands/fanta" class="btn btn-primary" role="button">Find out more...</a></p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="__STATIC__/index/kkk.png" alt="..." width="60%" height="30%" style="margin-left: 30px">
+                <div class="caption" style="width: 60%;padding-left: 30px;margin-top: 10px;margin-bottom: 50px">
+                    <p style=""><h5>Powerade</h5>I have updated the packaging of Powerade, hoping that Powerade can develop functional drinks with stronger resilience and can be used as the packaging of new products.</p>
+                    <p style="padding-left: 30px"><a href="https://www.coca-cola.co.uk/brands/powerade" class="btn btn-primary" role="button">Find out more...</a></p>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="__STATIC__/index/card3.png" alt="..." width="60%" height="30%" style="margin-left: 30px">
+                <div class="caption" style="width: 60%;padding-left: 30px;margin-top: 10px;margin-bottom: 50px">
+                    <p style=""><h5>Appletiser</h5>Appletiser has been expertly crafting its unique blend of 100% Natural Sparkling Apple Juice for over 50 years. It has no added sugar, coloring or preservatives.</p>
+                    <p style="padding-left: 30px"><a href="https://www.coca-cola.co.uk/brands/appletiser" class="btn btn-primary" role="button">Find out more...</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- === Footer Section === -->
+<script src="__STATIC__/index/js/jquery-3.3.1.min.js"></script>
+<script src="__STATIC__/index/js/bootstrap.min.js"></script>
+<script src="__STATIC__/index/js/isotope-pkgd.min.js"></script>
+<script src="__STATIC__/index/js/magnific-popup.min.js"></script>
+<script src="__STATIC__/index/js/wow.min.js"></script>
+<script src="__STATIC__/index/js/owl.min.js"></script>
+<script src="__STATIC__/index/js/viewport.jquery.js"></script>
+<script src="__STATIC__/index/js/odometer.min.js"></script>
+<script src="__STATIC__/index/js/circle-progress.js"></script>
+<script src="__STATIC__/index/js/main.js"></script>
+</body>
+</html>
